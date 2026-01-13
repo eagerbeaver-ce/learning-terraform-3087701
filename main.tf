d7d3623a46e1fd9e214f510739a0f94a3ce39aec
eagerbeaver-ce <50137453+eagerbeaver-ce@users.noreply.github.com>
2026-01-13 14:14:49 +0100

    use version 9.1.0

--- main.tf
+++ main.tf
@@ -37,18 +37,23 @@ module "blog_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  version = "9.1.0"
   
   name = "blog"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier   = module.blog_vpc.public_subnets
-  target_group_arns     = module.blog_alb.target_group_arns
   security_groups       = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+}
+
+# Create a new ALB Target Group attachment
+resource "aws_autoscaling_attachment" "blog-alb-target-group" {
+  autoscaling_group_name = aws_autoscaling_group.blog-alb-target-group.id
+  lb_target_group_arn    = aws_lb_target_group.blog-alb-target-group.arn
 }
 
 module "blog_alb" {
